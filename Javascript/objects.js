8f44d6c498162858bb5139f227222856ab426df6
Kushal Vardhan <vardhank140102@gmail.com>
2025-02-15 20:23:00 +0530

    Objects

--- Javascript/objects.js
+++ Javascript/objects.js
@@ -1,10 +1,15 @@
 const person ={
     firstName: 'Kushal',
     lastName: 'Vardhan',
-    hobby: ['Coding',"age"],
+    hobby: ['Coding',"Riding Sports Bike"],
     getFullNamd:function(){
         return `${this.firstName} ${this.lastName}`;
+    },
+    address:{
+        city: 'Mandi',
+        state: 'Himachal Pradesh'
     }
+
 }
 console.log(person.hobby);
 
