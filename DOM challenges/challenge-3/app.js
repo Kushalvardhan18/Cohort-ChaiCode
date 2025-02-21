35410f026c459876fd23de16971f576976e72920
Kushal Vardhan <vardhank140102@gmail.com>
2025-02-21 20:36:18 +0530

    challenge 3 : Real time updation of inputs

--- DOM challenges/challenge-3/app.js
+++ DOM challenges/challenge-3/app.js
@@ -1,3 +1,20 @@
-/**
- * Write your challenge solution here
- */
+const nameDisplay = document.querySelector('#nameDisplay')
+const jobDisplay = document.querySelector('#jobDisplay')
+const ageDisplay = document.querySelector('#ageDisplay')
+const BioDisplay = document.querySelector('#bioDisplay')
+
+const name = document.querySelector('#nameInput')
+const job = document.querySelector('#jobInput')
+const age = document.querySelector('#ageInput')
+const Bio = document.querySelector('#bioInput')
+
+function userInputs(input,output){
+    input.addEventListener('input',(e)=>{
+        output.textContent= e.target.value
+    })
+}
+
+userInputs(name,nameDisplay)
+userInputs(job,jobDisplay)
+userInputs(age,ageDisplay)
+userInputs(Bio,BioDisplay)
