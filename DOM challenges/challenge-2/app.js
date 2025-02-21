d256fdd657c5881e09fad02d60f5a84139cffc75
Kushal Vardhan <vardhank140102@gmail.com>
2025-02-21 19:00:57 +0530

    challenge 2 text color change

--- DOM challenges/challenge-2/app.js
+++ DOM challenges/challenge-2/app.js
@@ -1,3 +1,20 @@
-/**
- * Write your challenge solution here
- */
+const text = document.querySelector('#mainHeading')
+function colorChange(selectedColor) {
+    if(selectedColor === 'Reset'){
+        text.style.color = 'Black'
+    }
+    else{
+        text.style.color = selectedColor
+    }
+}
+const buttons = document.querySelectorAll('.color-buttons button')
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click', () => {
+        const selectedColor = button.innerText
+       
+        colorChange(selectedColor)
+        console.log(selectedColor)
+    })
+})
+
