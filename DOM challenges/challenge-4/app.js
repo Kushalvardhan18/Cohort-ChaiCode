db45a55c298bdd5aca0c8ece22f550c53c39551f
Kushal Vardhan <vardhank140102@gmail.com>
2025-02-21 21:57:47 +0530

    created a new li to add tasks

--- DOM challenges/challenge-4/app.js
+++ DOM challenges/challenge-4/app.js
@@ -1,3 +1,25 @@
-/**
- * Write your challenge solution here
- */
+let input = ""
+
+document.querySelector('#taskInput').addEventListener('input', (e) => {
+    input = e.target.value
+ 
+})
+
+document.querySelector('#addButton').addEventListener('click', () => {
+    addTask(input) 
+})
+
+
+function addTask(inputVal) {
+    let ul = document.querySelector('#taskList')
+
+    let newTask = document.createElement('li')
+
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute("type","checkbox")
+
+    newTask.appendChild(checkbox)
+    newTask.appendChild(document.createTextNode(inputVal))
+    ul.appendChild(newTask)
+    console.log(newTask);
+}
