97d96a2359f3795bd6e61bb3a64662b1180927a9
Kushal Vardhan <vardhank140102@gmail.com>
2025-02-22 18:53:32 +0530

    input field empty on adding task

--- DOM challenges/challenge-4/app.js
+++ DOM challenges/challenge-4/app.js
@@ -1,25 +1,41 @@
 let input = ""
+let count = 0
 
-document.querySelector('#taskInput').addEventListener('input', (e) => {
+const totalTask = document.querySelector('#totalTasks')
+const inputField =document.querySelector('#taskInput')
+inputField.addEventListener('input', (e) => {
     input = e.target.value
  
 })
 
-document.querySelector('#addButton').addEventListener('click', () => {
-    addTask(input) 
+const appendTask =document.querySelector('#addButton')
+appendTask.addEventListener('click', () => {
+    if(input === ""){
+        // appendTask.disabled = true;
+        alert("Task cannot be empty")
+
+    }
+    else{
+        count++;
+        addTask(input) 
+        countInc(count)
+        inputField.value =""
+        input =""
+    }
+    
 })
 
 
+function countInc(count){
+    console.log(count);
+    totalTask.innerText = `Total tasks: ${count}`
+}
 function addTask(inputVal) {
     let ul = document.querySelector('#taskList')
-
     let newTask = document.createElement('li')
-
     let checkbox = document.createElement("input")
     checkbox.setAttribute("type","checkbox")
-
     newTask.appendChild(checkbox)
     newTask.appendChild(document.createTextNode(inputVal))
     ul.appendChild(newTask)
-    console.log(newTask);
 }
