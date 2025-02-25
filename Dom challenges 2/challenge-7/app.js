ed86cf948b34edb427ad1b8fd8664afec4730048
Kushal Vardhan <vardhank140102@gmail.com>
2025-02-25 14:17:04 +0530

    challenge 7 completed

--- Dom challenges 2/challenge-7/app.js
+++ Dom challenges 2/challenge-7/app.js
@@ -1,3 +1,12 @@
-/**
- * Write your challenge solution here
- */
+let accordianItems = document.querySelectorAll('.accordion-item')
+accordianItems.forEach((item)=>{
+  item.addEventListener('click',()=>{
+    const isActive = item.classList.contains('active')
+    accordianItems.forEach((input)=>input.classList.remove('active'))
+
+    if(!isActive){
+
+        item.classList.add('active')
+    }
+  })
+})
