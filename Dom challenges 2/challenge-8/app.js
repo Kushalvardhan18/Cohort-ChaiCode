420deb1f7135aafd4c94a0b2b326ba00934bb252
Kushal Vardhan <vardhank140102@gmail.com>
2025-02-27 21:39:32 +0530

    quantity control buttons

--- Dom challenges 2/challenge-8/app.js
+++ Dom challenges 2/challenge-8/app.js
@@ -1,79 +1,66 @@
-// let count =0
-// const cart = document.querySelector("#cart-items")
-// const emptyCart = document.querySelector(".empty-cart")
-// const cartItem = document.createElement('div')
-// const qualityControl = document.createElement('div')
+let count =0
+function addToCart(itemName, price) {
+    // if(cart.contains(cartItem.it)){
+    //     quantityInc()
+    // }
+    const products = document.querySelectorAll(".product")
+    const cart = document.querySelector("#cart-items")
+    const emptyCart = document.querySelector(".empty-cart")
 
+    // created a div for adding the items details and class is added for easy selections. 
+    const cartItem = document.createElement('div')
+    cartItem.classList.add('cart-item')
 
-// // const removeButton = document.createElement('button')
-// function addToCart(itemName, price,image) {
-//    count++;
-//     qualityControl.classList.add("quantity-controls")
-//     cartItem.classList.add('cart-item')
-//     cartItem.appendChild(qualityControl)
-//     // cartItem.appendChild(removeButton)
-//     cartItem.append(itemName)
-//     cartItem.append(price)
-//     cart.appendChild(cartItem)
-//     cart.removeChild(emptyCart)
+    // created a div for adding the items details and class is added for easy selections. 
+    const qualityControl = document.createElement('div')
+    qualityControl.classList.add("quantity-controls")
 
-
-//     const quantityIncBtn = document.createElement('button')
-//     quantityIncBtn.innerText = "+"
-//     quantityIncBtn.addEventListener('click',()=>{
-//         count = count+1
-//         updateCount(count)
-//     })
-
-//     const quantityDecBtn = document.createElement('button')
-//     quantityDecBtn.innerText = "-"
-//     quantityDecBtn.addEventListener('click',()=>{
-//         count > 0 ?count =count-1 : cart.removeChild(cartItem)
-//         updateCount(count)
-
-//     })
-//     const removeBtn = document.createElement('button')
-//     removeBtn.innerText = "remove"
-//     const itemImage = document.createElement('img')
-//     qualityControl.appendChild(quantityIncBtn)
-//     qualityControl.appendChild(itemImage)
-//     qualityControl.appendChild(quantityDecBtn)
-//     qualityControl.appendChild(removeBtn)
-// }
-
-
-// const products = document.querySelector('.products')
-// const product =products.querySelectorAll('.product img')
-
-// console.log(product);
-
-// // product.forEach((input)=>{
-// //     console.log(input);
-// //     // console.log(img.src);
-
-
-
-// // })
-const cart = document.querySelector("#cart-items")
-const products = document.querySelectorAll(".product")
-const cartItem = document.createElement('div')
-const qualityControl = document.createElement('div')
-
-function addToCart(itemName, price, image) {
-let itemImage = document.createElement('img')
-
+    // Craeted a Image tag for the logo image and selecting src  of image for the logo display
+    let itemImage = document.createElement('img')
+    itemImage.style.width = "50px"
+    // Src of the Image for the logo
     products.forEach((product) => {
         const productName = product.querySelector("h3").innerText
         if (productName === itemName) {
-           itemImage.src = product.querySelector("img").src
-            console.log(itemImage);
+            itemImage.src = product.querySelector("img").src
+            // console.log(itemImage);
         }
     })
+
+
+    const quantityIncBtn = document.createElement('button')
+    quantityIncBtn.innerText = "+"
+    quantityIncBtn.addEventListener('click', () => {
+        console.log(count);
+        
+       quantityInc(count)
+    })
+
+    const quantityDecBtn = document.createElement('button')
+    quantityDecBtn.innerText = "-"
+    quantityDecBtn.addEventListener('click', () => {
+        count > 0 ? count = count - 1 : cart.removeChild(cartItem)
+        // updateCount(count)
+
+    })
+    const removeBtn = document.createElement('button')
+    removeBtn.innerText = "remove"
+    removeBtn.addEventListener('click',()=>{
+        cart.removeChild(cartItem)
+    })
     cart.appendChild(cartItem)
-    cartItem.classList.add('cart-item')
     cartItem.appendChild(qualityControl)
     cartItem.append(itemName)
     cartItem.append(price)
-    qualityControl.classList.add("quantity-controls")
+    qualityControl.appendChild(quantityIncBtn)
     qualityControl.appendChild(itemImage)
+    qualityControl.appendChild(quantityDecBtn)
+    qualityControl.appendChild(removeBtn)
+    cart.removeChild(emptyCart)
+
+}
+
+function quantityInc(){
+    count = count + 1
+    // updateCount(count)
 }
