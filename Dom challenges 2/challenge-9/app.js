2a823d4d4db0e49853e8b31aeb53a7f22e80888f
Kushal Vardhan <vardhank140102@gmail.com>
2025-02-28 14:54:25 +0530

    Challenge 9 completed

--- Dom challenges 2/challenge-9/app.js
+++ Dom challenges 2/challenge-9/app.js
@@ -1,3 +1,31 @@
-/**
- * Write your challenge solution here
- */
+const toggleBtn = document.querySelector(".toggle-btn")
+const panel = document.querySelector(".panel")
+const panelButtons = document.querySelectorAll(".panel .menu-item")
+const closeBtn = document.querySelector(".close-btn")
+
+// togge button functionality
+toggleBtn.addEventListener('click',(event)=>{
+    panel.classList.toggle('active')
+    event.stopPropagation()
+})
+
+// panel buttons access and functionality
+panelButtons.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click',()=>{
+        alert(`${button.innerText} Button is Clicked`)
+        panel.classList.toggle('active')
+    })   
+})
+
+// close btn functionality
+closeBtn.addEventListener("click",()=>{
+    panel.classList.remove('active')
+})
+
+// If Panel is active and anywhere in document is clicked then it will close
+document.addEventListener("click", (event) => {
+    if (panel.classList.contains("active")) {
+        panel.classList.remove("active");
+    }
+});
