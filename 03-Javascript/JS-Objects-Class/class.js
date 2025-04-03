e41a6a9c760dd402d9c291ac6399fa8bf6385dff
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-03 14:19:46 +0530

    class ,new keyword and constructor

--- 03-Javascript/JS-Objects-Class/class.js
+++ 03-Javascript/JS-Objects-Class/class.js
@@ -1,17 +1,17 @@
-class person  {
-    constructor(fname,lname){
-       this.fname = fname;
-       this.lname = lname;
+class person {
+    constructor(fname, lname) {
+        this.fname = fname
+        this.lname = lname
     }
     getFullName(){
-        return `${this.fname}  ${this.lname}`;
+        return `${this.fname} ${this.lname}`
     }
-
 }
-const p1 = new person("Kushal","Vardhan");
-const p2 = new person("Anirudh","Jawala");
-const p3 = new person("Piyush","garg");
+
+const p1 = new person("Kushal","vardhan")
+const p2 = new person("Aman","vardhan")
+console.log(p1);
+console.log(p2);
+
 console.log(p1.getFullName());
 console.log(p2.getFullName());
-console.log(p3.getFullName());
-
