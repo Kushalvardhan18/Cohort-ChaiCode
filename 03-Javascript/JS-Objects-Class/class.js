2652da984d5ce7265f82aabc5f81b2e359eb3495
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-04 20:29:22 +0530

    inheritance in js

--- 03-Javascript/JS-Objects-Class/class.js
+++ 03-Javascript/JS-Objects-Class/class.js
@@ -7,9 +7,14 @@ class person {
         return `${this.fname} ${this.lname}`
     }
 }
-
+class student extends person{
+    
+}
 const p1 = new person("Kushal","vardhan")
 const p2 = new person("Aman","vardhan")
+const s1 = new student("kk","vv")
+console.log(s1.getFullName());
+
 console.log(p1);
 console.log(p2);
 
