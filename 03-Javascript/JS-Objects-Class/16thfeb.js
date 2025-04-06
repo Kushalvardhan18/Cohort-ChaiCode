2d54ac75a32253a38e499bb066181e6266da4f50
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-06 13:03:24 +0530

    function

--- 03-Javascript/JS-Objects-Class/16thfeb.js
+++ 03-Javascript/JS-Objects-Class/16thfeb.js
@@ -7,3 +7,48 @@ function greet(){
     
 }
 greet()
+
+
+// Function Declaration
+function sum(a,b){
+return a+b
+}
+
+// Function Expression
+const sub = function(a,b){
+    return a-b
+}
+
+// Arrow Function
+const multiply = (a,b)=>{return a*b}
+
+console.log(sum(4,5));
+console.log(sub(4,5));
+console.log(multiply(4,5));
+
+// First class functions 
+function applyOperation(a,b, operation){
+    return operation(a,b)
+}
+
+const result = applyOperation(4,2, (x,y)=>x-y)
+console.log(result);
+
+
+function createCounter(){
+    let count =0
+    return function(){
+        count++;
+        return count
+    }
+}
+
+const counter = createCounter()
+console.log(counter());
+
+
+// IIFE - Immediately Invoked Function Expression 
+
+(function(){
+    console.log("Kushal");
+})()
