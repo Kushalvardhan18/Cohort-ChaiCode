6b961118bad5201b69dc220d891f2897d1bedc39
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-08 11:09:41 +0530

    iterations

--- 03-Javascript/JS-Objects-Class/22thfeb.js
+++ 03-Javascript/JS-Objects-Class/22thfeb.js
@@ -4,14 +4,75 @@ let name = "Kushal"
 let number = 10 // Number
 let str = "Kushal" // String
 let bool = true // Boolean
-let pi =3.14 // Number
+let pi = 3.14 // Number
 let n = null // Null // Object
 let x = undefined //Undefined
 
-let arr =[] // Object Array
-let obj ={} // Object
+let arr = [1, 2, 3, 4, 5] // Object Array
+let obj = {
+    name: "Kushal",
+    age: 23
+} // Object
 
 let symbolVar = Symbol()  // Symbol
 
-console.log(typeof(n));
+// console.log(typeof (n));
+
+let num = "a42a"
+// let convertedNum = +num
+// let convertedNum = parseInt(num)
+let convertedNum = Number(num)
+
+// console.log(convertedNum);
+// console.log(typeof (convertedNum));
+
+
+let str1 = 123
+let convertedString = String(str1)
+
+// console.log(typeof (convertedString));
+
+
+let a = 10
+let b = 3
+let sum = a + b
+let difference = a - b
+let product = a * b
+let quotient = a / b
+let remainder = a % b
+let power = a ** b
+
+
+let s = 5
+let y = 10
+// console.log(s == y);
+// console.log(s != y);
+// console.log(s < y);
+// console.log(s > y);
+// console.log(s >= y);
+// console.log(s >= y);
+
+// console.log(Math.min(s,y))
+// console.log(Math.max(s,y))
+// console.log(Math.random())
+
+let firstName = "Aman"
+let lastName = "Vardhan"
+// let fullName = firstName+lastName
+let fullName = `${firstName} ${lastName}`
+console.log(fullName);
+
+ function checkTruthyValue(value){
+    if(value){
+        console.log("Truthy");
+    }
+    else{
+        console.log("Falsy");
+    }
+ }
+checkTruthyValue(1)
+checkTruthyValue(0)
+checkTruthyValue([])
+checkTruthyValue("Kushal")
+checkTruthyValue("")
 
--- 03-Javascript/JS-Objects-Class/22thfebIteration.js
+++ 03-Javascript/JS-Objects-Class/22thfebIteration.js
@@ -3,8 +3,35 @@ let salesData = [
     { product: "Smarthone", price: 800 },
     { product: "Headphone", price: 120 },
     { product: "Keyboard", price: 10 },
+];
+
+let initialValue = 0;
+let totalSales = salesData.reduce(
+    (acc, sale) => acc + sale.price,
+    initialValue
+);
+console.log(totalSales);
+
+let inventory = [
+    { name: "Widget A", stock: 30 },
+    { name: "Widget B", stock: 120 },
+    { name: "Widget C", stock: 45 },
+    { name: "Widget D", stock: 70 },
+];
+
+// Output from filter is always an array.
+let lowStockItems = inventory.filter((item) => {
+    return item.stock < 50;
+});
+console.log(lowStockItems);
+
+
+let userActivity = [
+    { user: "Alice", activityCount: 45 },
+    { user: "Bob", activityCount: 72 },
+    { user: "Charlie", activityCount: 33 },
 ]
 
-let initialValue = 0
-let totalSales = salesData.reduce((acc ,sale) => (acc + sale.price), initialValue)
-console.log(totalSales);
+let mostAcitveUser = userActivity.reduce((maxUser, user) => user.activityCount > maxUser.activityCount ? user : maxUser)
+console.log(mostAcitveUser);
+
