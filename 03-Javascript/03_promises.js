d27c04d866265641e6f60fb57251d33254fe711f
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-14 21:58:18 +0530

    async and await

--- 03-Javascript/03_promises.js
+++ 03-Javascript/03_promises.js
@@ -40,8 +40,25 @@ function unlinkWithPromise(filepath) {
     })
 }
 
+// Multiple async code is running in sync fashion
 readFileWithPromise('./hello.txt', 'utf-8')
     .then((content) => writeFileWithPromise("./backup.txt", content))
     .then(() => unlinkWithPromise("./hello.txt"))
     .catch((e) => console.log("Error", e))
     .finally(() => console.log("Everything is done"))
+
+
+async function doTasks() {
+    try {
+        const fileContent = await readFileWithPromise('./hello.txt', 'utf-8')
+        await writeFileWithPromise("./backup.txt", fileContent)
+        await unlinkWithPromise("./hello.txt")
+        console.log("All done");
+    }
+    catch (err) {
+        console.log('Error', e);
+    } finally {
+        console.log("All Done");
+    }
+}
+doTasks()
