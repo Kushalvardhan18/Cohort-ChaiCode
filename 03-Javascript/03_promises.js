517916044d4c4e6ebd35bce4b93020d0e8b5ec22
Kushal Vardhan <vardhank140102@gmail.com>
2025-04-16 12:00:05 +0530

    pollyfills

--- 03-Javascript/03_promises.js
+++ 03-Javascript/03_promises.js
@@ -62,3 +62,4 @@ async function doTasks() {
     }
 }
 doTasks()
+ 
---
+++ 03-Javascript/machine_coding/polyfills2.js
@@ -0,0 +1,30 @@
+const arr = [1, 2, 3, 4, 5]
+
+// map -- for every ele run a cb and return a new array.
+if (!Array.prototype.myMap) {
+    Array.prototype.myMap = function (cb) {
+        const result = []
+        for (let i = 0; i < this.length; i++) {
+            result.push(cb(this[i]))
+        }
+        return result
+    }
+}
+const trippledArray = arr.myMap((e) => e * 3)
+console.log(trippledArray);
+
+
+// forEach -- Array ke harr ele k liye cb ko run krta hai
+if (!Array.prototype.myForEach) {
+    Array.prototype.myForEach = function (cb) {
+        for (let i = 0; i < this.length; i++) {
+            cb(this[i], i)
+        }
+    }
+}
+arr.myForEach((value, index) => {
+    console.log(`At index:${index} Value:${value}`);
+
+})
+
+// Promise
