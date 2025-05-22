564c3d10a18939996a4be70436c5372bbf516294
Kushal Vardhan <vardhank140102@gmail.com>
2025-05-22 11:07:10 +0530

    useState and useEffect

--- 06-React/06react/src/App.jsx
+++ 06-React/06react/src/App.jsx
@@ -1,19 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 export function App() {
-    const [message,setMessage]=useState("Loading.......") // [var,fn]
+  const [message, setMessage] = useState("Loading......."); // [var,fn]
 
-    useEffect( ()=>{
-        fetch(`http://localhost:3000/api`).then((res)=>res.json)
-        .then((data)=>setMessage(data.message))
-        .catch(()=>setMessage("Failed to load"))
-    } ,[])
+  useEffect(() => {
+    fetch(`https://api.freeapi.app/api/v1/public/randomjokes`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+        console.log(data);
+      })
+      .catch(() => setMessage("Failed to load"));
+  }, []);
 
-    // let anotherMessage  // cannot change in react -- react doesnt allow it to change.
-    return (
-        <div>
-            <h1>Welcome to Chaicode</h1>
-            <p>Serving hot chai with react</p>
-            <h2>{message}</h2>
-        </div>
-    )
+  // let anotherMessage  // cannot change in react -- react doesnt allow it to change.
+  return (
+    <div>
+      <h1>Welcome to Chaicode</h1>
+      <p>Serving hot chai with react</p>
+      <h2>{message}</h2>
+    </div>
+  );
 }
